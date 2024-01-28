aae1a548f3ae10bd1882fa2b124eb9589ecc8433
Rodion Mern <114694164+rodionmern@users.noreply.github.com>
2024-01-28 09:56:46 +0300

    update Foot.js, and Index.html

--- src/foot.js
+++ src/foot.js
@@ -1,14 +1,14 @@
 var footerCopyleft;
 
-function Copyleft1() {
+function firstCopyleft() {
 	footerCopyleft = document.getElementById('footer__copyleft').innerHTML = "Rofionenko(RodionMern) | Non Copyright"	
-} function Copyleft2() {
+} function secondCopyleft() {
 	footerCopyleft = document.getElementById('footer__copyleft').innerHTML = "Copyleft (c) 2024 | RodionMern(Rofionenko)"	
-} function CopyleftDefault() {
+} function defaultCopyleft(){
 	footerCopyleft = document.getElementById('footer__copyleft').innerHTML = "Copyleft (c) RodionMern (Rofionenko)"	
 }
 
-function Footer() {
+function footerCopyleft() {
 	footerCopyleft = document.getElementById('footer__copyleft').innerHTML = "(C)opyleft 2024 / Rofionenko(RodionMern)";
-	setTimeout(Copyleft1, 2500); setTimeout(Copyleft2, 5000); setTimeout(CopyleftDefault, 7500)
+	setTimeout(firstCopyleft, 2500); setTimeout(secondCopyleft, 5000); setTimeout(defaultCopyleft, 7500)
 }
